c1537ad589f624208f2ce85ec95d26c0bbeb6de1
Kesha Shah <kesha.shah1106@gmail.com>
2019-08-20 16:43:30 +0530

    R-Shiny App Basic

---
+++ calculator/ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Mercedes Benz Dealership Sales Employee Attrition Impact Analysis"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+    )
+    
+  )
+  
+)
+      
