e3db2efa8b619cd058eb3d41bca6f01d7c221373
Kesha Shah <kesha.shah1106@gmail.com>
2019-08-20 16:48:47 +0530

    Effects tabs skeleton

--- calculator/server.R
+++ calculator/server.R
@@ -1,3 +1,33 @@
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
+  
+  # Output for Tab-0 i.e. industry
+  output$industry <- renderText({
+    "Show industry here"
+  })  
+  
+  # Output for Tab-1 i.e. bottomline
+  output$bottomline <- renderText({
+    "Show bottomline here"
+  })  
+  
+  # Output for Tab-2 i.e. sales
+  output$sales <- renderText({
+    "Show sales here"
+  })  
+  
+  # Output for Tab-3 i.e. customers
+  output$customers <- renderText({
+    "Show customers here"
+  })  
+  
+  # Output for Tab-4 i.e. employees
+  output$employees <- renderText({
+    "Show employees here"
+  })  
+  
+  # Output for Tab-4 i.e. employees
+  output$stock <- renderText({
+    "Show stock here"
+  })  
 }
--- calculator/ui.R
+++ calculator/ui.R
@@ -10,10 +10,30 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
+      # Include clarifying text ----
+      helpText("The industry average for sales turnover in automobile industry dealership is 60%."),
+      helpText("Previous Attrition Rate of Mercedes Benz Dealers Across Germany is 40%."),
+      
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "attritionRate",
+                  label = "New Attrition Rate:",
+                  min = 1,
+                  max = 100,
+                  value = 20)
+      
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
+      # Output: Tabset w/ plot, summary, and table ----
+      tabsetPanel(type = "tabs",
+                  tabPanel("Industry Comparision", br(), verbatimTextOutput("industry")),
+                  tabPanel("Bottom-Line (MB)",br(), verbatimTextOutput("bottomline")),
+                  tabPanel("Sales (MB)", br(), verbatimTextOutput("sales")),
+                  tabPanel("Customers (MB)", br(), verbatimTextOutput("customers")),
+                  tabPanel("Employees (MB)", br(), verbatimTextOutput("employees")),
+                  tabPanel("Stock (MB)", br(), verbatimTextOutput("stock"), htmlOutput("stock1"))
+      )
     )
     
   )
